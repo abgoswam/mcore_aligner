e2e808206c06f3754f66434d40076eb2310a75fa
Abhishek Goswami <abgoswam@gmail.com>
2024-10-27 03:08:04 +0000

    setting up test files. next: try 200k tokenizer

--- toolkits/model_checkpoints_convertor/phi_T01/hf_test_model.py
+++ toolkits/model_checkpoints_convertor/phi_T01/hf_test_model.py
@@ -1,14 +1,17 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # Load the tokenizer and model
-model_name = "/mnt/syntheticpipelinetrainerv1/mcore_posttrain_v1/ckpts_base/mistral_ckpts/Mistral-7B-v0.1/"
+# model_name = "/mnt/syntheticpipelinetrainerv1/mcore_posttrain_v1/ckpts_base/mistral_ckpts/Mistral-7B-v0.1/"
+model_name = "./temp"
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, ignore_mismatched_sizes=True, trust_remote_code=True)
 
 # Move the model to the GPU
 device = "cuda"  # or "cuda:0" if you have multiple GPUs and want to specify the first one
 model.to(device)
+print(model)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
 # Encode the input prompt and move it to the GPU
 prompt = "Once upon a time"
